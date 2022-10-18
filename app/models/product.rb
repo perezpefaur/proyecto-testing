ae22da4a33e0cf04fd786b3c8b9e483e82f4819e
juanpacl111 <juan.contreras@uc.cl>
2022-10-18 20:09:31 -0300

    Validations ready

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,36 @@
 class Product < ApplicationRecord
+    validates :price, presence: true
+
+    validate :is_valid_category
+    validate :comestible_has_weight
+    validate :bebestible_has_volume
+
+    def is_valid_category
+        if category != "Bebestible" and category != "Comestible" and category != "Souvenir"
+            errors.add(:category, "no es una categoría válida")
+            # print recieved value
+        end 
+    end
+
+    def comestible_has_weight
+        if category == "Comestible"
+            if not weight.is_a? Numeric or not weight > 0
+                errors.add(:weight, "debe tener peso mayor a 0 si es comestible")
+            end
+        end
+    end
+
+    def bebestible_has_volume
+        if category == "Bebestible"
+            if not volume.is_a? Numeric or not volume > 0
+                errors.add(:volume, "debe tener volumen mayor a 0 si es bebestible")
+            end
+        end
+    end
+
+
+
+
+
+
 end
