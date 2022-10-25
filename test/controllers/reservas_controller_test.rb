b62097763d52aff47d17268ec33009ad4aa1be1b
perezpefaur <perezpefaur@uc.cl>
2022-10-25 20:36:34 -0300

    reservas controller 100% covarage

--- test/controllers/reservas_controller_test.rb
+++ test/controllers/reservas_controller_test.rb
@@ -15,10 +15,43 @@ class ReservasControllerTest < ActionDispatch::IntegrationTest
     MovieTime.destroy_all
     Movie.destroy_all
   end
+
   test 'Posting a new reserva' do
     assert_difference 'Reserva.count' do
       post new_reserva_url(5, '2000-11-12', 'TANDA'),
            params: { reservation_seats: 'C-3', name: 'Diego' }
     end
+  end
+
+  test 'Should get new' do
+    get new_reserva_url(5, '2000-11-12', 'TANDA')
+    assert_response :success
+  end
+
+  test 'Should not get new if name is empty' do
+    assert_no_difference 'Reserva.count' do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: 'C-3', name: '' }
+    end
+  end
+
+  test 'Should not get new if seat is full' do
+    Reserva.create(sala: 5, fecha: Date.new(2000, 11, 12), asiento: 3,
+                   horario: 'TANDA', name: 'Diego')
+    assert_no_difference 'Reserva.count' do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: 'A-3', name: 'Diego' }
+    end
+  end
+
+  test 'Should not get new if seat is empty' do
+    assert_no_difference 'Reserva.count' do
+      post new_reserva_url(5, '2000-11-12', 'TANDA'),
+           params: { reservation_seats: '', name: 'Diego' }
+    end
+  end
+
+  test 'get_asiento_value(item) should return the correct value' do
+    assert_equal 34, get_asiento_value('C-10')
   end
 end
