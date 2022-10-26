28b8b51b159b3a17f947d7c4ccb267b5ca356336
perezpefaur <perezpefaur@uc.cl>
2022-10-25 22:15:01 -0300

    100% covarage

--- test/controllers/movie_controller_test.rb
+++ test/controllers/movie_controller_test.rb
@@ -30,20 +30,27 @@ class MovieControllerTest < ActionDispatch::IntegrationTest
   test 'should create movie time' do
     movie = Movie.create(title: 'Matrix')
     assert_difference 'MovieTime.count' do
-      post new_movie_time_url, params: { movie_time:{
+      post new_movie_time_url, params: { movie_time: {
         room: 5, date_start: Date.new(2022, 10, 10),
-                                    date_end: Date.new(2022, 10, 12),
-                                    time: 'TANDA', movie_id: movie.id }}
+        date_end: Date.new(2022, 10, 12),
+        time: 'TANDA', movie_id: movie.id
+      } }
     end
   end
 
   test 'should not create movie time if room is empty' do
     movie = Movie.create(title: 'Matrix')
     assert_no_difference 'MovieTime.count' do
-      post new_movie_time_url, params: { movie_time:{
+      post new_movie_time_url, params: { movie_time: {
         room: '', date_start: Date.new(2022, 10, 10),
-                                    date_end: Date.new(2022, 10, 12),
-                                    time: 'TANDA', movie_id: movie.id }}
+        date_end: Date.new(2022, 10, 12),
+        time: 'TANDA', movie_id: movie.id
+      } }
     end
+  end
+
+  test 'should get movies list by date' do
+    get movies_by_date_url
+    assert_response :success
   end
 end
